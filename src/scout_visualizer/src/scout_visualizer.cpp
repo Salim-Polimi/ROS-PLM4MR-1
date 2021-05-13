65c5385ca887b76942c51a4e706d391222c87b51
Tinel21 <nicolasmarti98@gmail.com>
2021-05-13 19:00:21 +0200

    le ruote girano

--- src/scout_visualizer/src/scout_visualizer.cpp
+++ src/scout_visualizer/src/scout_visualizer.cpp
@@ -57,7 +57,7 @@ int main(int argc, char** argv)
 {
   ros::init(argc, argv, "scout_visualizer_node");
   
-  VisualizerNode Visualizer;
+  VisualizerNode Visualizer; 
 
 
   ros::Rate loop_rate(100); //100Hz
